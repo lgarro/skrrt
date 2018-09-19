c479484f0a96a0e4be34b47c95702c8c63e27fba
Luis Garro <luis@foundationlab.co>
2018-09-19 10:07:54 -0600

    lint hotfix

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies,no-console */
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
 import BackendServer from './backend.server'
 
 const server = new BackendServer()
-const connectDatabase = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
-        console.log('Mongo connected')
-    } catch (e) {
-        console.log('Fatal error connecting Mongo database', e)
-    }
-}
+// const connectDatabase = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+//         console.log('Mongo connected')
+//     } catch (e) {
+//         console.log('Fatal error connecting Mongo database', e)
+//     }
+// }
 const init = async () => {
     try {
         // await connectDatabase()
